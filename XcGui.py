7acab746017ce5bfd633eaffc379bb4101a8092b
fnoyanisi <fnoyanisi@yahoo.com>
2017-08-18 22:45:59 +1200

    Designed Gui layout - still yet to implement command functions

--- XcGui.py
+++ XcGui.py
@@ -13,35 +13,61 @@ GUI Layout for xc-converter
     |                    button                     |
     +-----------------------------------------------+
 """
-
 from tkinter import *
 
-def OptSelection():
-    s = selection.get()
-    label3.config(text=s)
+class XcGuiApplication():
+    def __init__(self, top):
+        version = 'v0.1'
+        self.top = top
+        top.wm_title('xc-converter ' + version)
+        top.resizable(width=False, height=False)
+        top.geometry('{}x{}'.format(400, 200))
 
-top=Tk()
+        label1 = Label(top, text='Xml to Csv Converter ' + version, font="bold")
+        label1.grid(row=0, column=0, sticky=W + E + N + S, padx=5, pady=10)
 
-label1 = Label(top, text = "Xml to Csv Converter", font="bold")
-label1.grid(row=0, column=0, sticky=W)
+        label2_text = 'Please select\n' \
+                      '    * The type of operation\n' \
+                      '    * The input file\n' \
+                      '    * The output directory '
+        label2 = Label(top, text=label2_text, justify=LEFT)
+        label2.grid(row=1, column=0, sticky=W)
 
-label2_text = 'Please select\n' \
-          '    * The type of operation\n' \
-          '    * The input file\n' \
-          '    * Output directory '
-label2 = Label(top, text = label2_text, justify=LEFT)
-label2.grid(row=1, column=0, sticky=W)
+        # Operation Selections
+        selection = StringVar()
+        opt1 = Radiobutton(top, text='XML to CSV Conversion', variable=selection, value='x2c', command=self.OptSelection)
+        opt1.grid(row=2, column=0)
 
-# Operation Selections
-selection = StringVar()
-opt1 = Radiobutton(top, text = 'XML to CSV Conversion', variable=selection, value='x2c', command=OptSelection)
-opt1.grid(row=2, column=0)
+        opt2 = Radiobutton(top, text='CSV to XML Conversion', variable=selection, value='c2x', command=self.OptSelection)
+        opt2.grid(row=3, column=0)
 
-opt2 = Radiobutton(top, text = 'CSV to XML Conversion', variable=selection, value='c2x', command=OptSelection)
-opt2.grid(row=3, column=0)
+        button_run = Button(top, text='    Run    ', command=self.FileChooser)
+        button_run.grid(row=4, column=0, columnspan=2, sticky=W + E + N + S, padx=5, pady=5)
 
-label3_text='test'
-label3 = Label(top,text=label3_text)
-label3.grid(row=4, column=0)
+        button_fc = Button(top, text='Select input file', command=self.FileChooser)
+        button_fc.grid(row=0, column=1, sticky=W + E, padx=5, pady=5)
 
-top.mainloop()
+        label3_text = 'No file selected'
+        label3 = Label(top, text=label3_text)
+        label3.grid(row=1, column=1)
+
+        button_dc = Button(top, text='Select output directory', command=self.DirChooser)
+        button_dc.grid(row=2, column=1, sticky=W + E + N + S, padx=5, pady=5)
+
+        label4_text = 'No directory selected'
+        label4 = Label(top, text=label4_text)
+        label4.grid(row=3, column=1)
+
+    def OptSelection(self):
+        s = self.selection.get()
+        self.label3.config(text=s)
+
+    def FileChooser(self):
+        pass
+
+    def DirChooser(self):
+        pass
+
+root = Tk()
+xc_gui = XcGuiApplication(root)
+root.mainloop()
