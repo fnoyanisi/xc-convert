aabcd3cfe215e843be7b4ed2a55ecc5297f3134f
Fehmi Noyan Isi <fnoyanisi@yahoo.com>
2021-08-13 16:10:55 +1200

    additions to ManagedObject and List

---
+++ src/managedobject.py
@@ -0,0 +1,37 @@
+"""
+represents a managedObject entry in an XML file of the form
+     <managedObject class="..." version="....." distName="...." id="....">
+       <p name="xxx">...</p>
+       <p name="yyy">...</p>
+       <p name="zzz">...</p>
+       <list name="aaaa">
+           <item>
+               <p name="xxx">...</p>
+               ....
+           </item>
+       </list>
+       ....
+     </managedObject>
+"""
+import xmlentry
+
+
+class List(xmlentry):
+    def __init__(self, n):
+        super().__init__(n + '{}')
+
+
+class ManagedObject(xmlentry):
+    # uses XmlEntry.name as the moClass value
+    moClass = ""
+    version = ""
+    distName = ""
+    id = ""
+
+    # constructor
+    def __init__(self, n, v, d, i):
+        super().__init__(n)
+        self.version = v
+        self.distName = d
+        self.id = i
+        self.moClass = self.name
