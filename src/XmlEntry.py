1e22a501c845f45b1a06edda6fdc273855e32b87
Fehmi Noyan Isi <fnoyanisi@yahoo.com>
2021-08-13 13:43:56 +1200

    separate module for XmlEntry

---
+++ src/XmlEntry.py
@@ -0,0 +1,30 @@
+# base class for more complex entries in the XML file
+# this can be subclassed by a managed object or a list
+class XmlEntry:
+    name = ""
+    propertyNames = []
+    propertyValues = {}
+
+    def __init__(self, n):
+        self.name = n
+
+    def add_properties(self, p):
+        self.propertyValues = p
+        for key in p:
+            self.propertyNames.append(key)
+
+    def add_property(self, pname, pval):
+        self.propertyNames.append(pname)
+        self.propertyValues[pname] = pval
+
+    def has_property(self, pname):
+        if pname in self.propertyNames:
+            return True
+        else:
+            return False
+
+    def get_property(self, pname):
+        if pname in self.propertyNames:
+            return self.propertyValues[pname]
+        else:
+            return None
--- src/managedObject.py
+++ src/managedObject.py
@@ -12,37 +12,7 @@
 #       ....
 #     </managedObject>
 #
-
-# base class for compex entries in the XML file
-# this could be a managed object or a list
-class XmlEntry:
-    name = ""
-    propertyNames = []
-    propertyValues = {}
-
-    def __init__(self, n):
-        self.name = n
-
-    def add_properties(self, p):
-        self.propertyValues = p
-        for key in p:
-            self.propertyNames.append(key)
-
-    def add_property(self, pname, pval):
-        self.propertyNames.append(pname)
-        self.propertyValues[pname] = pval
-
-    def has_property(self, pname):
-        if pname in self.propertyNames:
-            return True
-        else:
-            return False
-
-    def get_property(self, pname):
-        if pname in self.propertyNames:
-            return self.propertyValues[pname]
-        else:
-            return None
+import XmlEntry
 
 
 class List(XmlEntry):
