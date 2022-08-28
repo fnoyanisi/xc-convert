fbd7d38791af342243982b2db55eb5352e1e5b8e
Fehmi Isi <fnoyanisi@yahoo.com>
2022-08-28 12:37:37 +1200

    using an enum to distinguish different XmlEntry types

--- src/xmlentry.py
+++ src/xmlentry.py
@@ -1,8 +1,23 @@
 # base class for more complex entries in the XML file
-# this can be subclassed by a managed object or a list
+# this can be subclassed by a managed object, a list or an item
+from enum import Enum
+
+
+# enumeration type to distinguish different types of XmlEntries
+class Type(Enum):
+    PARAMETER = 1
+    ITEM = 2
+    LIST = 3
+
+# n -   name of the XML entry. For parameters, this is the internal name of the
+#       parameter. For lists, this is the internal name of the list appended with
+#       '{}' characters at the end to indicate this is a list item.
+#       No names, or empty string, is used for item entries.
+# t -   this parameter should be of type Types
 class XmlEntry:
-    def __init__(self, n):
+    def __init__(self, n, t):
         self.name = n
+        self.type = t
         self.propertyNames = []
         self.propertyValues = {}
 
