fd5fa5b84147a651a6e41a82548c8469c101f675
Fehmi Isi <fnoyanisi@yahoo.com>
2022-09-23 10:23:35 +1200

    change cwd

--- run.bat
+++ run.bat
@@ -2,5 +2,6 @@
 echo DO NOT CLOSE THIS WINDOW!
 echo.
 echo starting xc-convert...
-call "C:\Program Files\Anaconda3\condabin\activate.bat" activate 
-python src\xc-convert.py
+call "C:\Program Files\Anaconda3\condabin\activate.bat" activate
+cd src
+python xc-convert.py
