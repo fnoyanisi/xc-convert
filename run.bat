32b8454841cf975b8fecde4f422e568409271a6f
Fehmi Isi <fnoyanisi@yahoo.com>
2022-10-05 11:48:37 +1300

    added new Python interpreter for Windows

--- run.bat
+++ run.bat
@@ -1,7 +1,32 @@
 @echo off
+
+:: Skip the function definitions
+goto Run
+
+:: Function definitions
+:DisplayHeader
+setlocal
 echo DO NOT CLOSE THIS WINDOW!
 echo.
 echo starting xc-convert...
-call "C:\Program Files\Anaconda3\condabin\activate.bat" activate
-cd src
-python xc-convert.py
+exit /b 0
+
+
+:Run
+:: Check a few possible locations for a 
+:: Python interpreter
+
+if exist "C:\Program Files\Python\Python39" (
+	call :DisplayHeader
+	"C:\Program Files\Python\Python39\python.exe" src\xc-convert.py
+	exit
+)
+
+if exist "C:\Program Files\Anaconda3\condabin\activate.bat" (
+	call :DisplayHeader
+	call "C:\Program Files\Anaconda3\condabin\activate.bat" activate 	
+	python src\xc-convert.py
+	exit
+)
+
+echo NO PYTHON INTERPRETER IS FOUND. EXISTING...
