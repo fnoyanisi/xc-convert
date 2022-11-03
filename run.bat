bcb34ebcef69a374577e88370328a89297b762f4
Fehmi Isi <fnoyanisi@yahoo.com>
2022-11-03 22:36:03 +1300

    added PYTHONPATH

--- run.bat
+++ run.bat
@@ -11,22 +11,24 @@ echo.
 echo starting xc-convert...
 exit /b 0
 
+set PYTHONPATH=%cd%
 
 :Run
-:: Check a few possible locations for a 
+:: Check a few possible locations for a
 :: Python interpreter
 
 if exist "C:\Program Files\Python\Python39" (
 	call :DisplayHeader
-	"C:\Program Files\Python\Python39\python.exe" src\xcc.py
+	"C:\Program Files\Python\Python39\python.exe" src\xc-convert.py
 	exit
 )
 
 if exist "C:\Program Files\Anaconda3\condabin\activate.bat" (
 	call :DisplayHeader
-	call "C:\Program Files\Anaconda3\condabin\activate.bat" activate 	
-	python src\xcc.py
+	call "C:\Program Files\Anaconda3\condabin\activate.bat" activate
+	python src\xc-convert.py
 	exit
 )
 
 echo NO PYTHON INTERPRETER IS FOUND. EXISTING...
+endlocal
