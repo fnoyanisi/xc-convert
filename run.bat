8d15600dd808fc1f3c717ed8a9dd77baa136b54d
Fehmi Isi <fnoyanisi@yahoo.com>
2022-11-03 22:57:01 +1300

    fixed module paths

--- run.bat
+++ run.bat
@@ -13,20 +13,37 @@ exit /b 0
 
 set PYTHONPATH=%cd%
 
+:Run@echo off
+
+:: Skip the function definitions
+goto Run
+
+:: Function definitions
+:DisplayHeader
+setlocal
+echo DO NOT CLOSE THIS WINDOW!
+echo.
+echo starting xc-convert...
+exit /b 0
+
 :Run
 :: Check a few possible locations for a
 :: Python interpreter
 
 if exist "C:\Program Files\Python\Python39" (
 	call :DisplayHeader
-	"C:\Program Files\Python\Python39\python.exe" src\xc-convert.py
+	set PYTHONPATH=%cd%
+	cd src
+	"C:\Program Files\Python\Python39\python.exe" xcc.py
 	exit
 )
 
 if exist "C:\Program Files\Anaconda3\condabin\activate.bat" (
 	call :DisplayHeader
+	set PYTHONPATH=%cd%
 	call "C:\Program Files\Anaconda3\condabin\activate.bat" activate
-	python src\xc-convert.py
+	cd src
+	python xcc.py
 	exit
 )
 
